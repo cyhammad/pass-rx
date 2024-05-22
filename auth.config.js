baa2b70475eecbb8f1ed562738021f94313e441d
cyhammad <hammad727272@gmail.com>
2024-05-23 01:09:50 +0500

    merging

---
+++ auth.config.js
@@ -0,0 +1,19 @@
+export const authConfig = {
+  pages: {
+    signIn: "/auth/sign-in",
+    callbacks: {
+      authorized({ auth, request: { nextUrl } }) {
+        const isLoggedIn = !!auth?.user;
+        const isOnDashboard = nextUrl.pathname.startsWith("/admin");
+        if (isOnDashboard) {
+          if (isLoggedIn) return true;
+          return false; // Redirect unauthenticated users to login page
+        } else if (isLoggedIn) {
+          return Response.redirect(new URL("/admin", nextUrl));
+        }
+        return true;
+      },
+    },
+    providers: [],
+  },
+};
